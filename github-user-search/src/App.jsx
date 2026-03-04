579abc1835044250b0bc71340ae84d2be94dd7cb
Brunokayitar <brunokayitare85@gmail.com>
2026-03-04 20:44:36 +0200

    Initialize GitHub User Search project with Vite, Axios, and environment setup

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <p>Setup complete. Ready to build!</p>
+    </div>
+  );
+}
+
+export default App;
+
+export default App
