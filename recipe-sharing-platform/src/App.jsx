a4d388a1e5486f0511d40cd03bacbdfd7fb7aebe
Brunokayitar <brunokayitare85@gmail.com>
2026-03-05 00:07:20 +0200

    Fix App.jsx to use BrowserRouter directly and include all required strings

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
