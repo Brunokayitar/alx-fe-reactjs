0818ab35095352fca9815b690d72bde51f06703a
Brunokayitar <brunokayitare85@gmail.com>
2026-03-05 04:24:55 +0200

    Add Jest global comment and duplicate test file

--- react-todo/src/TodoList.test.js
+++ react-todo/src/TodoList.test.js
@@ -1,5 +1,6 @@
+/* global describe, test, expect, jest */
 import { render, screen, fireEvent } from '@testing-library/react';
-import TodoList from '../components/TodoList';
+import TodoList from './components/TodoList';
 
 describe('TodoList', () => {
   test('renders initial todos', () => {
@@ -13,10 +14,8 @@ describe('TodoList', () => {
     render(<TodoList />);
     const input = screen.getByTestId('todo-input');
     const addButton = screen.getByTestId('add-button');
-
     fireEvent.change(input, { target: { value: 'New todo' } });
     fireEvent.click(addButton);
-
     expect(screen.getByText('New todo')).toBeInTheDocument();
   });
 
@@ -24,7 +23,6 @@ describe('TodoList', () => {
     render(<TodoList />);
     const todoText = screen.getByTestId('todo-text-1');
     fireEvent.click(todoText);
-
     const todoItem = screen.getByTestId('todo-item-1');
     expect(todoItem).toHaveStyle('text-decoration: line-through');
   });
@@ -33,7 +31,6 @@ describe('TodoList', () => {
     render(<TodoList />);
     const deleteButton = screen.getByTestId('delete-button-1');
     fireEvent.click(deleteButton);
-
     expect(screen.queryByText('Learn React')).not.toBeInTheDocument();
   });
 });
--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,3 +1,4 @@
+/* global describe, test, expect, jest */
 import { render, screen, fireEvent } from '@testing-library/react';
 import TodoList from '../components/TodoList';
 
